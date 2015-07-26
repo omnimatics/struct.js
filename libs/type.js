2af23fab6bebbb8f560d038db975a5792e3f7cd1
Raj Kissu <rajkissu@gmail.com>
2015-07-26 14:31:24 +0800

    0.0.6
    * adds test case for `libs/type`

--- libs/type.js
+++ libs/type.js
@@ -2,7 +2,7 @@
 
 function BYTE(n) { n = n || 1; return 1 * n; }
 function WORD() { return BYTE(2); }
-function DWORD() { return WORD * 2; }
+function DWORD() { return WORD() * 2; }
 
 const DataTypes = { 
   BYTE  : BYTE,
